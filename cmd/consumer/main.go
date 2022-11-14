0005c1688822cfb7380683175bd633189d1f2620
Anderson Souza <andersonszdc@gmail.com>
2022-11-14 07:40:51 -0300

    producer criado

--- cmd/consumer/main.go
+++ cmd/consumer/main.go
@@ -4,6 +4,8 @@ import (
 	"database/sql"
 	"encoding/json"
 	"fmt"
+	"net/http"
+	"time"
 
 	"github.com/andersonszdc/my-orders/internal/order/infra/database"
 	"github.com/andersonszdc/my-orders/internal/order/usecase"
@@ -28,7 +30,26 @@ func main() {
 	out := make(chan amqp.Delivery) // channel
 	go rabbitmq.Consume(ch, out)    // T2
 
-	for msg := range out {
+	qtdWorkers := 5
+	for i := 0; i < qtdWorkers; i++ {
+		go worker(out, &uc, i)
+	}
+
+	http.HandleFunc("/total", func(w http.ResponseWriter, r *http.Request) {
+		getTotalUC := usecase.GetTotalUseCase{OrderRepository: repository}
+		total, err := getTotalUC.Execute()
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
+		}
+		json.NewEncoder(w).Encode(total)
+	})
+
+	http.ListenAndServe(":8080", nil) // Chama o server HTTP, cria uma thread
+}
+
+func worker(deliveryMessage <-chan amqp.Delivery, uc *usecase.CalculateFinalPriceUseCase, workerID int) {
+	for msg := range deliveryMessage {
 		var inputDTO usecase.OrderInputDTO
 		err := json.Unmarshal(msg.Body, &inputDTO)
 		if err != nil {
@@ -39,6 +60,7 @@ func main() {
 			panic(err)
 		}
 		msg.Ack(false)
-		fmt.Println(outputDTO)
+		fmt.Printf("Worker %d has processed order %s\n", workerID, outputDTO.ID)
+		time.Sleep(1 * time.Second)
 	}
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/andersonszdc/my-orders
 go 1.19
 
 require (
+	github.com/google/uuid v1.3.0
 	github.com/mattn/go-sqlite3 v1.14.16
 	github.com/rabbitmq/amqp091-go v1.5.0
 	github.com/stretchr/testify v1.8.1
